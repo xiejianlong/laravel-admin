97c7f6d550f2c8d9c7ec97ebe0963504a38cb423
xiejl <Lxznds00>
2019-05-10 10:27:43 +0800

    创建库

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,8 +12,6 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('car',CarInfoController::class);
-    /*$router->get('/car', 'CarInfoController@index')->name('admin.car');
-    $router->get('/car/create', 'CarInfoController@create')->name('admin.car.create');
-    $router->get('/examine', 'CarExamineController@index')->name('admin.examine');*/
+
 
 });
