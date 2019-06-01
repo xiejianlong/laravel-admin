f8948e922b6319ed29b580396e7e50288d7e8639
xiejianlong <xjl199306162834>
2019-06-01 12:54:57 +0800

    201961

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,7 +10,7 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
 
-    //$router->get('/', 'HomeController@index')->name('admin.home');
+    $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('car','CarInfoController');
     $router->resource('examine','CarExamineController');
 
